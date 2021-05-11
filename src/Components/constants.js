f998d207829a71b7818a9155be4fa8c67bb381d5
nickolasz523 <z.nickolas523@gmail.com>
2021-05-10 23:44:24 -0400

    joe

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -1,3 +1,6 @@
+import styled from 'styled-components';
+
+
 export const PageTransition = {
     in: {
         opacity: 1,
@@ -9,17 +12,12 @@ export const PageTransition = {
     }
 };
 
-export const activeStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'auto',
-    margin: 0,
-    width: '100%',
-    position: "absolute",
-    background: '#131313',
-    top: '100%',
-    opacity: 1,
-    transition: 'all 0.5s ease',
-    zIndex: 99,
-    height: '60vh',
-    fontSize: '1.6rem',
-}
+export const LinksWrapper = styled.ul`
+    background: #131313;
+    top: 100%;
+    opacity: 1;
+    transition: all 0.5s ease;
+    z-index: 99;
+    height: 60vh;
+    font-size: 1.6rem;
+`
--- src/Components/navbarHeader.js
+++ src/Components/navbarHeader.js
@@ -4,19 +4,37 @@ import {PageTransition, activeStyle} from './constants'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 
+
 export default function NavbarHeader() {
 	const [showMenu, setShowMenu] = useState(false)
     return (
-        <motion.div exit = {PageTransition.out} animate ={PageTransition.in} initial ={PageTransition.out} variants = {PageTransition}>
+        <motion.div exit = {PageTransition.out} animate ={PageTransition.in}
+		 initial ={PageTransition.out} variants = {PageTransition}>
         	<nav class="navbar">
 				<div class="navbar__container">
 					<a href="/home"id="navbar__logo">NICKOLAS ZAMACHNOI</a>
 					<div class="navbar__toggle" id="mobile-menu">
 						<span>
-							<FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)}/>
+							<FontAwesomeIcon icon={faBars}
+							 onClick={() => setShowMenu(!showMenu)}/>
 						</span>
 					</div>
-					<ul class="navbar__menu">
+					{showMenu ? 
+					<ul class="navbar__menu" >
+						<li class="navbar__item">
+							<a href="/home" class="navbar__links" id="home-page">Home</a>
+						</li>
+						<li class="navbar__item">
+							<a href="/about" class="navbar__links" id="about-page">About</a>
+						</li>
+						<li class="navbar__item">
+							<a href="/work" class="navbar__links" id="work-page">Work</a>
+						</li>
+						<li class="navbar__btn">
+							<a href="/contact" class="button" id="contact">Contact</a>
+						</li>
+					</ul>:
+					<ul class="navbar__menu" style={{top: -1000}}>
 						<li class="navbar__item">
 							<a href="/home" class="navbar__links" id="home-page">Home</a>
 						</li>
@@ -30,6 +48,8 @@ export default function NavbarHeader() {
 							<a href="/contact" class="button" id="contact">Contact</a>
 						</li>
 					</ul>
+					
+					}
 				</div>
 			</nav>
         </motion.div>
