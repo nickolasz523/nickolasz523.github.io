9dc50cdf6e61fda1e4e0971c288fde3ab4ee7a90
nickolasz523 <z.nickolas523@gmail.com>
2022-10-10 00:05:35 -0400

    updated for 2022

--- src/Components/about.js
+++ src/Components/about.js
@@ -25,27 +25,22 @@ export default function about() {
 					</div>
 					<div class="main__content">			
 						<h1>WHO I AM</h1>
-						<p>I am Nickolas Zamachnoi, a grade 12 student at the Ottawa-Carleton Virtual Secondary School. 
-							I'm an aspiring software developer and am going into my 1st year at Carleton University for a Bachelor of Computer Science. 
-							I love coding and want to make it my job in the future. I work on my skills every day to become the best I can be.</p>
+						<p>Having worked as an Information Technology User Support Assistant at the City of Ottawa, I have experience in the tech industry and knowledge of how to complete deliverables so that a project can be executed to its fullest potential. 
+						In my free time, I have created a handful of projects and learned languages on my own such as React.js to enhance my skills and the look of my portfolio website. 
+						Also, having worked at Camp Smitty as a Senior Counsellor has helped me develop my interpersonal, leadership, and communication skills. 
+						As a Senior Counsellor it was my job to collaborate with my Junior Counsellor, and my superiors to identify problems and implement solutions as a team, and an individual.</p>
 						<br></br>
-						<h1>WHY I'M HERE</h1>
-						<p>I love working with my brain and solving problems.
-							I challenge myself everyday to at least 1 coding problem so that I keep improving and never stagnate.
-							Making beautiful looking websites has always been a goal of mine. I want my projects to be functional but also look pretty.
-							The languages which I am comfortable with right now are:<br></br>
+						<h1>Highlights of Qualifications</h1>
+						<p>
 							<ul>
-								<li>Java</li>
-								<li>Python</li>
-								<li>C</li>
-								<li>CSS</li>
-								<li>HTML</li>
-								<li>JavaScript</li>
+								<li>Excelled at meeting targets, and consistently surpassed them over an 8-month period at VF Corporation.</li><br></br>
+								<li>Leadership skills gained as a Senior Counselor at Camp Smitty by mentoring youth and Junior counsellors.</li><br></br>
+								<li>Sound judgement demonstrated by experiencing high intensity and dynamic situations and consistently provided key solutions to problems at Camp Smitty.</li><br></br>
+								<li>Fluent in French and Russian: oral, written, and reading </li><br></br>
+								<li>Fostered a positive workplace environment for my colleagues – helping improve morale and keeping focused on tasks.</li>
 							</ul>
 							</p>
 						<br></br>
-						<h1>WHERE I'M GOING</h1>
-						<p>In the future I hope</p>
 						<button class="main__btn"><a href="/contact" id="contact">Contact</a></button>
 					</div>
 				</div>
--- src/Components/home.js
+++ src/Components/home.js
@@ -5,23 +5,11 @@ import ParticleBackground from './ParticleBackground'
 
 export default function home() {
     return (
-        <motion.div exit = {PageTransition.out} animate ={PageTransition.in} initial ={PageTransition.out} variants = {PageTransition} style={{background: "#120019"}} class="poop">
-            {/* <ParticleBackground className="homer" style={{background: "purple", position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "1%"
-        }}></ParticleBackground> */}
-            <div class="home-section" id="home" style={{
-            // position: "absolute",
-            // top: 80,
-            // left: 0,
-            width: "100%",
-            height: "100%",
-          }}>
+        <motion.div exit = {PageTransition.out} animate ={PageTransition.in} initial ={PageTransition.out} variants = {PageTransition} style={{background: "#120019"}} class="homepage">
+            <div class="home-section" id="home">
                 <div class="home__container">
-                    <h1 class="home__heading">Nickolas's <span>Portfolio</span></h1>
-                    <p class="home__description">Future Full-Stack and<br></br>Software Developer</p>
+                    <h1 class="home__heading">Nickolas <span>Zamachnoi</span></h1>
+                    <p class="home__description">Full-Stack and<br></br>Software Developer</p>
                     <button class="main__btn"><a href="/about">Explore</a></button>
                 </div>
             </div>
